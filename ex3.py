ff21d9f53c8a33b3ef13132d1775a7e34a71033a
basema-k <basema.khan1@ucalgary.ca>
2026-03-02 12:41:38 -0700

    ex3

--- ex3.py
+++ ex3.py
@@ -1,1 +1,26 @@
+import sys
+import timeit
+
+# 1:
+# When arrays are full, 
+
+# 2:
+mylist = []
+prev_size = sys.getsizeof(mylist)
+
+for n in range(64):
+    mylist.append(n)
+    new_size = sys.getsizeof(mylist)
+
+    if new_size != prev_size:
+        print("Capcity changed: ", new_size, "bytes")
+        prev_size = new_size
+
+# 3:
+slist = []
+
+
+
+
+
 
